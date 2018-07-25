060a5116b3c46b74bd1aa21c53806a1bed4ed475
GAN JIA MIN ALVINA <17010247@rp.edu.sg>
2018-07-25 15:22:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/contactslist/CustomAdapter.java
@@ -0,0 +1,50 @@
+package sg.edu.rp.c346.contactslist;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+
+
+    Context parent_context;
+    int layout_id;
+    ArrayList<Contacts> contactList;
+
+    public CustomAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Contacts> objects) {
+        super(context, resource, objects);
+        this.parent_context = context;
+        this.layout_id = resource;
+        this.contactList = objects;
+    }
+
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater)parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(layout_id, parent, false);
+        TextView tvName = rowView.findViewById(R.id.tvName);
+        TextView tvCode = rowView.findViewById(R.id.tvCountryCode);
+        TextView tvNum = rowView.findViewById(R.id.tvPhoneNum);
+
+        Contacts currentItem = contactList.get(position);
+        tvName.setText(currentItem.getName());
+        tvCode.setText(currentItem.getCountryCode());
+        tvNum.setText(currentItem.getPhoneNum());
+
+        return rowView;
+
+    }
+}
+
+
+
+
